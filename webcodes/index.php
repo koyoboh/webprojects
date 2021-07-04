0514399740492d95a2cb885d08996534f44a7dfa
koyoboh <67437693+koyoboh@users.noreply.github.com>
2021-07-04 19:27:53 +0100

    Add files via upload

--- webcodes/index.php
+++ webcodes/index.php
@@ -1,1 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width,
+	initial-scale=1">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/art.css">
+	<meta name="description" content="Wedding Designer For Weddings">
+	<title>Wedding Designer</title>
+	<title></title>
+</head>
 
+<!--Navbar-->
+<div class="topnav" id="myTopnav">
+
+	<a href="#" class="active">Home</a>
+	<a href="#">Designs</a>
+	<a href="#">Portfolio</a>
+	<a href="#">Contact</a>
+	<img src="image/rose.jpg" style="max-width: 100px; height: auto;">
+
+
+	<a href="javascript:void(0);" class="icon" onclick="myMenu()";><i class="fa fa-bars"></i></a>
+
+
+</div>
+
+
+
+
+
+	<div class="sociallinks">	
+			<p>
+			<a href="https://www.facebook.com/thenisda" class="fa fa-facebook" id="Socials" style="font-size: 30px; text-decoration: none; padding: 10px 10px;
+			color: #be2327; margin: 5px 2px;" ></a>
+			<a href="https://www.instagram.com/thenisda" id="Socials" class="fa fa-instagram" style="font-size: 30px; text-decoration: none; padding: 10px 10px;
+			color: #be2327; margin: 5px 2px;"></a>
+			<a href="https://www.twitter.com/thenisda" class="fa fa-twitter" id="Socials" style="font-size: 30px; text-decoration: none; padding: 10px 10px;
+			color: #be2327; margin: 5px 2px;"></a>
+			
+		 	</p>
+		</div>
+
+
+
+<div class="row">
+	<div class="side1">
+		<img src="image/rose.jpg" class="inner-image" alt="image">
+		<h1>Company Name</h1>
+		<h2>New Designs</h2>
+		<li>2021</li>
+		<li>2020</li>
+		
+	</div>
+
+	<div class="main">
+		<h1>The Best Service</h1>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+
+		<h1>The Best Product</h1>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<h1>More Info</h1>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+
+
+		
+		
+		
+	</div>
+		
+		
+	</div>
+	
+
+</div>
+<div class="bottom">Footer</div>
+<footer>
+
+
+		<div class="footer-row">
+
+			<div class="footer1">
+				<h4>Column1</h4>
+				<small>Insert Links</small><br>
+				<small>Insert Links</small><br>
+				
+			</div>
+
+			<div class="footer2">
+				<h4>Column2</h4>
+				<small>Insert Links</small><br>
+				<small>Insert Links</small><br>
+				
+			</div>
+
+			<div class="footer3">
+				<h4>Column3</h4>
+				<small>Insert Links</small><br>
+				<small>Insert Links</small>
+
+				
+			</div>
+			
+		</div>
+
+
+
+	
+</footer>
+
+<script src="art.js"></script>
+<body>
+
+</body>
+</html>
