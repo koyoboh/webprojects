65ee1a1e50147cecf4df9d8b1c9064133e590ab2
koyoboh <67437693+koyoboh@users.noreply.github.com>
2021-07-04 20:00:49 +0100

    Add files via upload

---
+++ livewebsite/index.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="a75efb87-b197-4804-89a5-a42b64c7d473" data-blockingmode="auto" type="text/javascript"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/strencardio.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Khula' rel='stylesheet'>
+	<link rel = "shortcut icon" href ="image/sfav.png"  type = "image/png">
+
+	<title>Personal Trainer In West Drayton | Uxbridge | London</title>
+	<meta name="description" content="Personal Training, Couple Training, Group Training, Nutrition and Wellness Counselling In Hillingdon, London ">
+
+
+</head>
+<body>
+	<header></header>
+	
+	
+	<a href="index.php"><img src="image/strencardiowhite.png" class="headerlogo"  alt="strencaridio logo" >  </a>
+
+	<div class="topnav" id="myTopnav">
+		
+
+		<a href="index.php" class="active">Home</a>
+		<a href="page/about.php">About</a>
+		<a href="page/services.php">Services</a>
+		<a href="page/sixweekprogram.php">6 Weeks Program</a>
+		<a href="page/contact.php">Contact</a>
+		<a href="page/shop.php">Shop</a>
+
+			<div class="dropdown">
+			<button class="dropbtn">Blogs <i class="fa fa-caret-down"></i></button>
+			<div class="dropdown-content">
+				<a href="blog/water.php">Water</a>
+				<a href="blog/importanceofcarbohydrates.php">Carbs</a>
+				<a href="blog/lowcalorieyogurtchocolatecake.php">Recipe</a>
+				<a href="blog/spicysalmon.php">Spicy Salmon</a>
+				
+			</div>
+			
+		</div>
+
+
+
+
+		<a href="javascript:void(0);" class="icon"; onclick="myMenu()";>
+					<i class="fa fa-bars"></i>
+				</a>
+				
+		
+		
+	</div>
+	<div style="background-color: #191919; color: #797b54; ; text-align: center; font-size: 25px; margin-top: 10px;">Health and Fitness </div>
+	<div class="header-banner"> 
+			<small>
+					<a href="https://www.facebook.com/strencardio" class="fa fa-facebook" id="socials" style="font-size: 30px; text-decoration: none; padding: 10px 10px;
+				color: #191919; margin: 5px 2px;" ></a>
+				<a href="https://www.instagram.com/_strencardio" id="socials" class="fa fa-instagram" style="font-size: 30px; text-decoration: none; padding: 10px 10px;
+				color: #191919; margin: 5px 2px;"></a>
+				<a href="https://www.twitter.com/strencardio" class="fa fa-twitter" id="socials" style="font-size: 30px; text-decoration: none; padding: 10px 10px;
+				color: #191919; margin: 5px 2px;"></a>
+					
+				</small>
+
+	</div>
+	<div class="sixp" style="background-color: #fff; color: #000; text-align: center;"><a href="page/sixweekprogram.php">Sign Up For My Six 6 Week Online Program?</a> </div>
+	<img src="image/rx3.png" alt="image"  style="width: 100%; height: auto;">
+	<img src="image/divpark3.png" alt="image"  style="width: 100%; height: auto;">
+
+
+	<div ><H1> <em>Strength, Endurance and Cardio Training..</em>  </H1></div>
+
+
+
+
+	<div class="row">
+		<div class="side">
+			<img src="image/divptm2.png" alt="group image" class="innerpic"  style="max-width: 75%; height: auto;"> <br>
+			<p style="font-size: 20px">Specialties:</p> 
+			
+			<div class="sidelist">
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Weight Loss</li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Fitness Test </li>
+						<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Body Composition</li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Strength Training</li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Endurance Training</li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Cardiovascular Training</li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Nutrition And Wellness  Counselling</li>
+
+				<img src="image/rx3.png" alt="image"  style="width: 100%; height: auto; ">	
+				</div>
+				
+			
+		</div>
+
+			<div class="main">
+			
+
+			<h1>STRENcardio Mindset </h1>
+		
+			<p>
+				STRENcardio is a lifestyle that promotes Health and Fitness. Whether you lack motivation, the desire to be physically active or you are simply unhappy about  your health. Always take this into consideration, It is never too late to start making changes to your way of life and alter bad habits. 
+				With over 12 years experience in the wellness industry, <b><a class="innerlink" href="page/services.php">STRENcardio Health & Fitness</b> </a>is the right destination for you. Our programs will be customized to you based on your fitness goals and nutrition needs. <br>
+
+				
+
+				
+
+			</p>
+		
+			
+			<h1>Personal Training</h1>
+			<h4>1 : 1</h4>
+			<img src="image/lo.jpg" alt="group image" class="innerpic-main" style="max-width: 75%; height: auto;">
+			
+			<p>
+				Personalized fitness program based on your needs and goals. After your free consultation and fitness assessment. Your program will be constructed and implemented in a careful and safe setting with proper form and technique. We all have different goals when it comes to fitness and weight loss. But with hard work and commitment, great things can be achieved with a dedicated mindset.
+
+			</p>
+			<button class="book" id="booknow" onclick="bookNow();"> Book Now  <i class='fa fa-envelope'></i></button>
+
+			<h1>Private Group Training</h1>
+			<h4>Group Training | Couple Training </h4>
+			<img src="image/stretch2.png" alt="group image" class="innerpic-main" style="max-width: 75%; height: auto;">
+			<p>
+				Private fitness class dedicated to you and your work colleagues, family members or partners This is a great way to strengthen your bond and collectively achieve your fitness goals. Be ready to push yourself and challenge yourself with great minds and also add varieties to your workout, especially when done in an outdoor setting.
+				Receive a FREE gift for you and your members once signed up.
+
+			</p>
+			<button class="book" id="booknow1" onclick="bookNow1();"> Book Now  <i class='fa fa-envelope'></i></button>
+			<script >
+				function bookNow1(){
+					var book1 = document.getElementById('booknow1') ;
+					book1.innerHTML= 'info@strencardio.co.uk';
+
+						}
+			</script>
+
+
+
+
+			
+
+
+			<!--Segments-->
+
+			<h1>Nutrition</h1>
+			<h4>Nutrition And Wellness Counselling </h4>
+			<img src="image/bd.png" alt="group image" class="innerpic-main" style="max-width: 75%; height: auto;">
+			<p>
+			If you are struggling with weight loss, have difficulties losing weight, lack the discipline to control what you consume or you simply find it hard to stop eating too much of the wrong things.<br> We can work together, talk about it and I will create a meal plan for you. Remember whatever you ingest is always processed and absorbed in your body. So it's always smart to know and have a brief understanding and comprehension of macronutrients and micronutrients.
+			
+
+			</p>
+				<a href="page/contact.php" class="innerlink">Free Consultation <i class='fa fa-envelope'></i></a>
+
+			<!--Segments-->
+
+
+			
+
+			<h1>STRENcardio Kids Fitness</h1>
+			<h4>Child Obesity </h4>
+			<img src="image/co.png" alt="group image" class="innerpic-main" style="max-width: 75%; height: auto;">
+			
+			<p>
+			
+			We live in an era where kids are becoming highly inactive. Society has changed drastically. There are many distractions, and sometimes plenty of school work. Parents don't have enough time to spend with their kids, sometimes due to long hours of work. <br>
+			The media, especially on the tv are constantly advertising poorly nutrient foods, which can be packed with lots of sugar, salt and highly saturated fat. <br>
+			According to RCPCH Almost 1 in 5 children are overweight or obese when they start primary school. It is also estimated that, half of all children will be overweight or obese by 2020. <small><em>source:</em> </small> <a href="https://www.rcpch.ac.uk" class="innerlink">rcpch.ac.uk</a><br>
+			IF YOUR CHILD IS OBESE OR OVERWEIGHT, PLEASE DO NOT HESITATE TO CONTACT ME, SO WE CAN DEVISE A PLAN. <br>
+			<small style="color: #fff">  I strongly advised to check with a medical professional first, incase there might be an underlying issue.</small>
+			
+
+			</p>
+				<a href="page/contact.php" class="innerlink">Free Consultation <i class='fa fa-envelope'></i></a>
+
+			<!--Segments-->
+
+			<h1>Weight Loss</h1>
+			<h4>STRENcardio 6-WEEK Weight Loss Program</h4>
+			<img src="image/yu.png" alt="group image" class="innerpic-main" style="max-width: 75%; height: auto;">
+			
+			<p>
+				<div>I invite you to sign up for my <a href="page/sixweekprogram.php" class="innerlink">my 6-WEEK Weight Loss Program.</a> This is a bespoke online program, so I will have to know a little bit about your health, your eating pattern and training frequency.</div> <br>
+
+				<div class="sidelist" style="text-align: left;">
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i>  WEEKLY Nutrition and Wellness Consultations.</li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Fitness Test </li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Free Workout Plan</li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Free Meal Plan</li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> Unique Progress Chart</li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #fff"></i> 30% OFF From Any Personal Training Package</li>
+					<img src="">	
+					<small style="color: #797b54;">  Please see more details  <a href="page/sixweekprogram.php" class="innerlink">Here</a></small>
+				</div>
+
+			
+			 <br>
+			 <!--Segments-->
+
+			 <h1>SHOP</h1>
+			<h4>FREE UK DELIVERY </h4>
+			<h4>Double Knit Cotton Beanie</h4>
+			<img src="image/beanie.jpg" alt="group image" class="innerpic-main" style="max-width: 75%; height: auto;">
+			<p>
+			100% cotton with a STRENcardio logo embroidery.<br>
+			Comfort wear, durable and soft. <br>
+			75gsm. <br>
+			Available in two colours <em><small>Navy | Black</small> </em> <br>
+			<a href="page/shop.php" class="innerlink">Buy Now <i class='fa fa-shopping-cart'></i></a>
+			</p>
+				
+
+		
+
+			<a href="index.php">
+			<img src="image/strencardiowhite.png" style="background-color: #191919; padding: 10px; max-width: 100% ;height: auto;" alt="group image" class="innerpic-main">
+			</a>
+			
+
+			</p>
+			
+			<a href="page/contact.php" class="innerlink">Book Now <i class='fa fa-envelope'></i></a>
+			<p>Note, Photos are taken before the pandemic and before current England lockdown measures. <br>
+							<strong>
+								Safety Always Comes First, Please Stay At Home, Protect Yourselt And Protect The NHS.
+  							</strong>
+							
+						 </p>
+
+			
+
+
+		</div>
+
+
+		
+	</div>
+
+
+	<div><H1>Your Health Is Your Greatest Asset</H1></div>
+
+	<footer>
+		<div class="footer-row">
+			<div class="footer-1">
+				<h4>Connect With STRENcardio</h4>
+				<button class="button-footer"; onclick="butn()" id="butn">See Email</button> <br>
+				<small>
+					<a href="https://www.facebook.com/strencardio" class="fa fa-facebook" id="socials" style="font-size: 30px; text-decoration: none; padding: 10px 10px;
+			color: #191919; margin: 5px 2px;" ></a>
+			<a href="https://www.instagram.com/_strencardio" id="socials" class="fa fa-instagram" style="font-size: 30px; text-decoration: none; padding: 10px 10px;
+			color: #191919; margin: 5px 2px;"></a>
+			<a href="https://www.twitter.com/strencardio" class="fa fa-twitter" id="socials" style="font-size: 30px; text-decoration: none; padding: 10px 10px;
+			color: #191919; margin: 5px 2px;"></a>
+					
+				</small> 
+				
+				
+			</div>
+
+			<div class="footer-2">
+				<h4>Disclaimer</h4>
+				<small>*DISCLAIMER | Results may vary from person to person | Results may happen at different timeframes | These results cannot be guaranteed - effort is required! Please consult with a medical professional before starting any new fitness or nutrition programme. ​</small>
+				
+			</div>
+
+			<div class="footer-3">
+				<h4>&copy; STRENcardio &hearts; 2021</h4>
+				<small>  Web Design | <a href="https://www.koyoboh.co.uk" style="color: #191919;"><small >KOYOBOH</small></a></small> 
+				
+
+				
+			</div>
+
+			<div class="footer-4">
+				<h4>Links</h4>
+				<small style="list-style: none;">
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #000"></i> <a href="blog/water.php" class="innerlink">Blogs</a> </li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #000"></i> <a href="page/services.php" class="innerlink">Services</a> </li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #000"></i> <a href="page/services.php" class="innerlink">Online Training</a> </li>
+					<li><i class="fa fa-arrow-circle-o-right" style="font-size:24px; color: #000"></i> <a href="page/shop.php" class="innerlink">Shop</a> </li> <br>
+					<a href="index.php"><img src="image/strencardioblack.png" alt="logo image" style="max-width: 50%; height: auto;"></a>
+					
+					
+				</small>
+				
+			</div>
+			
+		</div>
+	</footer>
+
+
+	
+<script src="strencardio.js"></script>
+
+
+</body>
+</html>
